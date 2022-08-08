0beee803f774273ec3d7c1f6c103c3c6ee9f737a
Cherrie <cherriekim925@gmail.com>
2022-08-08 17:53:09 +0900

    added #3

---
+++ 2_Develop/3_KthBigNumber.py
@@ -0,0 +1,21 @@
+'''
+채리는 1부터 100사이의 자연수가 적힌 N장의 카드를 가지고 있습니다. 
+같은 숫자의 카드가 여러장 있을 수 있습니다. 
+채리는 이 중 3장을 뽑아 각 카드에 적힌 수를 합한 값을 기록하려 합니다. 
+3장을 뽑을 수 있는 모든 경우를 기록합니다. 
+기록한 값 중 K번째로 큰 수를 출력하세요.
+만약 큰 수부터 만들어진 수가 25 25 23 23 22 20 19......이고 
+K값이 3이라면 K번째 큰 값은 22입니다.
+
+Cherrie has N number of cards 
+with natural numbers between 1 and 100 written on it.
+Threre may be multiple cards of the same number. 
+Cherrie wants to draw three of them and record 
+the sum of the numbers written on each card. 
+Record all the cases in which you can pull out three sheets. 
+Print the Kth largest number of recorded values.
+If the number produced from the large number is 
+25 25 23 22 20 19.... and the value of K is 3, 
+the Kth largest value is 22.
+'''
+
