076b5beafdf3501440a951c25c998c87ada29f95
Cherrie <cherriekim925@gmail.com>
2022-08-21 20:04:50 +0900

    solved #5

---
+++ 05_SumOfNumbers.py
@@ -0,0 +1,36 @@
+'''
+N개의 수로 된 수열 A[1], A[2], …, A[N] 이 있다. 
+이 수열의 i번째 수부터 j번째 수까지의 합 A[i]+A[i+1]+…+A[j-1]+A[j]가 
+M이 되는 경우의 수를 구하시오.
+
+There is a sequence  A[1], A[2], …, A[N] of N numerics. 
+Calculate the number of cases which the sum of the sequence's
+ith value to the jth value, A[i]+A[i+1]+...+A[j-1]+A[j], becomes M.
+'''
+
+'''
+마지막 조건 시간 제한 exceed 함.
+해결해보기!
+'''
+
+import sys
+#sys.stdin = open("input.txt", "rt")
+
+N, M = map(int, input().split())
+lst = list(map(int, input().split()))
+
+
+cnt = 0
+
+
+for i in range(N):
+    sum = 0
+    for j in range(i, N):
+        sum += lst[j]
+        if sum == M: 
+            cnt += 1
+            break
+        if sum > M:
+            break
+    
+print(cnt)
