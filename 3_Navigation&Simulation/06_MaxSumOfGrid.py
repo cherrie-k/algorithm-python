dab5b8136327ec5f363616408a925d50c9dc854c
Cherrie <cherriekim925@gmail.com>
2022-08-22 19:32:47 +0900

    The last commit was faulsy.. My mistake. Commiting the file with the correct codes. 'solved #6 for real...'

--- 3_Navigation&Simulation/06_MaxSumOfGrid.py
+++ 3_Navigation&Simulation/06_MaxSumOfGrid.py
@@ -7,3 +7,45 @@ the sum of each column, and the sum of two longest diagonal lines.
 Print the largest sum.
 '''
 
+import sys
+#sys.stdin = open("input.txt", "rt")
+
+N = int(input())
+lst = []
+for i in range(N):
+    lst.append(list(map(int, input().split())))
+
+max = 0
+
+# 행 더함
+for i in range(N):
+    sum = 0
+    for j in range(N):
+        sum += lst[j][i]
+    if sum > max:
+        max = sum;
+
+# 열 더함
+for i in range(N):
+    sum = 0
+    for j in range(N):
+        sum += lst[i][j]
+    if sum > max:
+        max = sum;
+
+# 좌측 상단에서 우측 하단으로 가는 대각선
+sum = 0
+for i in range(N):
+    sum += lst[i][i]
+if sum > max:
+    max = sum
+ 
+# 우측 상단에서 좌측 하단으로 가는 대각선       
+sum = 0       
+for i in range(N):
+    sum += lst[i][N - i - 1]
+if sum > max:
+    max = sum
+
+
+print(max)
