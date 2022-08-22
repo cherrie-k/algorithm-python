f3f39d34ff4d4df31f67e1d0d5e15408a95108cf
Cherrie <cherriekim925@gmail.com>
2022-08-22 19:15:23 +0900

    solved #6

---
+++ 3_Navigation&Simulation/06_MaxSumOfGrid.py
@@ -0,0 +1,9 @@
+'''
+N*N의 격자판이 주어지면 각 행의 합, 각 열의 합, 
+두 대각선의 합 중 가장 큰 합을 출력하세요.
+
+Given a grid of N*N, calculate the sum of each row, 
+the sum of each column, and the sum of two longest diagonal lines.
+Print the largest sum.
+'''
+
