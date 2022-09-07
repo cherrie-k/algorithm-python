4be3e62373f0269824e04c67118cfa1ab4721391
Cherrie <cherriekim925@gmail.com>
2022-09-07 22:36:24 +0900

    added another solution for #6

--- 3_Navigation&Simulation/06_MaxSumOfGrid.py
+++ 3_Navigation&Simulation/06_MaxSumOfGrid.py
@@ -49,3 +49,37 @@ if sum > max:
 
 
 print(max)
+
+
+'''
+'''
+
+import sys
+#sys.stdin = open("input.txt", "rt")
+n = int(input())
+a = [list(map(int, input().split())) for _ in range(n)]
+
+largest = -2147000000
+
+# 행과 열 합 한번에 구하기
+for i in range(n):  
+    sum1 = sum2 = 0
+    for j in range(n):
+        sum1 += a[i][j]    # j를 고정하고 i를 돌림
+        sum2 += a[j][i]    # i로 열이 고정되고 j가 돈다
+    if sum1 > largest:
+        largest = sum1
+    if sum2 > largest:
+        largest = sum2
+
+# 대각선 합도 한번에 구하기
+sum1 = sum2 = 0
+for i in range(n):
+    sum1 += a[i][i]
+    sum2 += a[i][n - i - 1]
+if sum1 > largest:
+    largest = sum1
+if sum2 > largest:
+    largest = sum2
+
+print(largest)
