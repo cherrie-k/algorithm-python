890fb317f06ae9a1f2707c570aa1f741dd8d1576
Cherrie <cherriekim925@gmail.com>
2022-09-07 22:56:49 +0900

    added another solution for #8

--- 3_Navigation&Simulation/08_HourGlassShape.py
+++ 3_Navigation&Simulation/08_HourGlassShape.py
@@ -70,3 +70,37 @@ for i in range(N):
 print(sum)
 
 
+'''
+
+import sys
+#sys.stdin = open("input.txt", "rt")
+n = int(input())
+a = [list(map(int, input().split())) for _ in range(n)]
+m = int(input())
+for i in range(m):
+    h, t, k = map(int, input().split())
+    if t == 0:
+        for _ in range(k):
+            a[h-1].append(a[h-1].pop(0))
+    else:
+        for _ in range(k):
+            a[h-1].insert(0, a[h-1].pop())
+    # ㅎㅎ 여기까진 내가 짠거랑 똑같당
+    
+res = 0
+s = 0
+e = n-1
+for i in range(n):
+    for j in range(s, e+1):
+        res += a[i][j]
+    if i<n//2:
+        s+=1
+        e-=1
+    else:
+        s-=1
+        e+=1
+# 이것도 똑같다ㅎㅎ 뿌듯~~
+
+print(res)
+
+'''
