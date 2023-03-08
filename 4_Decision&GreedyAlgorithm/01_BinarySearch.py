339cd385276eb0bb5710e2dd39216d786ca93536
Cherrie <cherriekim925@gmail.com>
2023-03-08 22:22:56 +0900

    added another solution for #01

--- 4_Decision&GreedyAlgorithm/01_BinarySearch.py
+++ 4_Decision&GreedyAlgorithm/01_BinarySearch.py
@@ -31,3 +31,25 @@ while flag:
         print(mid+1)
         flag = False
 
+
+'''
+다른 풀이
+import sys
+sys.stdin = open("input.txt", "rt")
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+a.sort()
+
+lt = 0
+rt = n-1
+while lt<=rt:
+    mid = (lt + rt) // 2
+    if a[mid] == m:     # 답 찾음
+        print(mid + 1)
+        break
+    elif a[mid] > m:
+        rt = mid -1
+    else: 
+        lt = mid + 1
+        
+'''
